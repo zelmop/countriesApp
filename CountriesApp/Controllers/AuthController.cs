7d6ad851a1ed934c9391bec826a778f76e341013
Zelmar Ramirez <zelmar.ramirez@gmail.com>
2019-01-01 20:18:12 -0300

    RESTAPI #06

--- CountriesApp/Controllers/AuthController.cs
+++ CountriesApp/Controllers/AuthController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
 
 using CountriesApp.Models;
+using CountriesApp.Services;
+
 
 namespace CountriesApp.Controllers
 {
@@ -10,7 +13,45 @@ namespace CountriesApp.Controllers
     [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
-        //private 
+        private readonly AuthService _authService;
+        private readonly UserService _userService;
+
+        public AuthController(AuthService authService, UserService userService)
+        {
+            _authService = authService;
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]User userParam)
+        {
+            var user = _authService.Auhtenticate(userParam.FirstName, userParam.Password);
+
+            if (user == null)
+            {
+                return BadRequest(new { message = "Username or password is incorrect" });
+            }
+            else
+            {
+                return Ok(user);
+            }
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> Get(string name)
+        {
+            var user = await _userService.GetUser(name);
+
+            if (user == null)
+            {
+                return new NotFoundResult();
+            }
+            else
+            {
+                return new ObjectResult(user);
+            }
+        }
 
     }
 }
